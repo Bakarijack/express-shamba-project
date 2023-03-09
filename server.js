3dbd35717fe87122fc490fb9db9f9c6598636f95
Bakarijack <bakarikilu254@gmail.com>
2023-03-09 09:49:36 +0300

    update the mysql library to mysql2

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 const app = require('./express')
 const port = process.env.PORT || 3000
-const mysql = require('mysql')
+const mysql = require('mysql2')
 
 // connect to the database
 
-// const con = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'Bakari@1',
-//     database: 'node_db'
-// })
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Bakari@1',
+    database: 'node_db'
+})
 
-// con.connect((error) => {
-//     if (error) throw error
+con.connect((error) => {
+    if (error) throw error
 
-//     console.log('connected!')
-// })
+    console.log('connected!')
+})
 
 app.listen(port, () => console.log(
     `http://localhost:${port}`
